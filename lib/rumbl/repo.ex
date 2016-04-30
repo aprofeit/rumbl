585ee092c9c6cd352909517f3826a22068000a52
Alexander Profeit <alex@profeit.com>
2016-04-30 18:11:40 -0400

    dry

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -12,7 +12,7 @@ defmodule Rumbl.Repo do
   def all(_module), do: []
 
   def get(module, id) do
-    Enum.find(all(module), fn map -> map.id == id end)
+    get_by(module, %{id: id})
   end
 
   def get_by(module, params) do
